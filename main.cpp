2aa9f978b9622a573f13ace82e64b4b75b107394
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-26 03:05:07 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//Zachary Hoppock
+//name.cpp
+//9/18/2020
+//SHow the output of a first name and last name added together in one string
+//Version #1
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  string first_name = "Kenneth";
+  string last_name = "Cole";
+  string name;
+  name = first_name + last_name;
+  cout << name;
+  return 0;
+}
